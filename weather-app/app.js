90f10356b25c5daff4fa0d29d813fee1e43f563b
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-05 13:38:48 -0700

    weather-app initial commit

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log("starting");
+
+setTimeout(() => {
+    console.log("2 seconds...");
+}, 2000);
+
+setTimeout(() => {
+    console.log("0 seconds...");
+}, 0);
+
+console.log("stopping");
