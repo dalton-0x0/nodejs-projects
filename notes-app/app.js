0f69f4b123a3df8579cfa571ea1d80cbd075d289
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-05 12:39:17 -0700

    using yargs to build command line iinputs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,66 @@
-const fs = require("fs");
+const yargs = require("yargs");
+const notes = require("./notes.js");
 
-fs.writeFileSync(
-    "notes.txt",
-    "trying to figure out node-js. it's pretty cool :)"
-);
+// Create add command
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string",
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body);
+    },
+});
 
-fs.appendFileSync("notes.txt", " and I just appended this extra text");
+// Create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler(argv) {
+        notes.removeNote(argv.title);
+    },
+});
 
-console.log("append successful!");
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse();
